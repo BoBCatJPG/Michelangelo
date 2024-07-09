63d5fa1c5391258a52b7eb652bcd7763f6b85fea
BoBCatJPG <manuellibero1999@gmail.com>
2024-07-09 10:11:23 +0200

    First true push

---
+++ michelangelo.py
@@ -0,0 +1,205 @@
+
+import discord
+from discord.ext import commands
+import yt_dlp as youtube_dl
+import asyncio
+import os
+from youtube_search import YoutubeSearch
+
+# Get the API token from the .env file.
+DISCORD_TOKEN = os.getenv('discord_token')
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!',intents=intents)
+
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',
+    'outtmpl': os.path.join('audio_files', '%(title)s.%(ext)s'),
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+#delete files in the project directory
+def delete_file(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+#converting using YoutubeSearch for easy to use in discord chat
+def search_youtube(query):
+    results = YoutubeSearch(query, max_results=1).to_dict()
+    if results:
+        return f"https://www.youtube.com{results[0]['url_suffix']}"
+    return None
+
+@bot.command(name='play', help='To play song')
+async def play(ctx,search :str):
+    try :
+        url = search_youtube(search)
+        print(url)
+        if url is None:
+            await ctx.send("Nenti truvai")
+        
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename), after=lambda e: delete_file(filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("Mbare, prima ma fari tràsiri.")
+
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{}, prima trasi tu e poi iu, chi spacchiu fai".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.pause()
+    else:
+        await ctx.send("vita mia sono già in coffe break.")
+    
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        voice_client.resume()
+    else:
+        await ctx.send("Ma si scemu, nun u viri ca nun staiu sunannu.")
+    
+
+
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        voice_client.disconnect()
+    else:
+        await ctx.send("Ma frate, prima ma fare trasìri.")
+
+@bot.command(name='stop', help='Stops the song')
+
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.stop()
+    else:
+        await ctx.send("Ma si scemu, nun u viri ca nun staiu sunannu, cose re pazzi.")
+
+
+@bot.event
+async def on_ready():
+    print('Ca semu!')
+    for guild in bot.guilds:
+        for channel in guild.text_channels :
+            if str(channel) == "general" :
+                await channel.send('Prontu sugnu..')
+                await channel.send(file=discord.File('giphy.png'))
+        print('Active in {}\n Member Count : {}'.format(guild.name,guild.member_count))
+
+@bot.command(help = "Prints details of Author")
+async def whats_my_name(ctx) :
+    await ctx.send('Ou mbare {}'.format(ctx.author.name))
+
+@bot.command(help = "Prints details of Server")
+async def where_am_i(ctx):
+    owner=str(ctx.guild.owner)
+    region = str(ctx.guild.region)
+    guild_id = str(ctx.guild.id)
+    memberCount = str(ctx.guild.member_count)
+    icon = str(ctx.guild.icon_url)
+    desc=ctx.guild.description
+    
+    embed = discord.Embed(
+        title=ctx.guild.name + " Server Information",
+        description=desc,
+        color=discord.Color.blue()
+    )
+    embed.set_thumbnail(url=icon)
+    embed.add_field(name="Owner", value=owner, inline=True)
+    embed.add_field(name="Server ID", value=guild_id, inline=True)
+    embed.add_field(name="Region", value=region, inline=True)
+    embed.add_field(name="Member Count", value=memberCount, inline=True)
+
+    await ctx.send(embed=embed)
+
+    members=[]
+    async for member in ctx.guild.fetch_members(limit=150) :
+        await ctx.send('Name : {}\t Status : {}\n Joined at {}'.format(member.display_name,str(member.status),str(member.joined_at)))
+
+    
+
+@bot.event
+async def on_member_join(member):
+     for channel in member.guild.text_channels :
+         if str(channel) == "general" :
+             on_mobile=False
+             if member.is_on_mobile() == True :
+                 on_mobile = True
+             await channel.send("Ma frate benvenuto {}!!\n: {}".format(member.name,on_mobile))             
+        
+# TODO : Filter out swear words from messages
+
+@bot.command()
+async def tell_me_about_yourself(ctx):
+    text = "My name is WallE!\n I was built by Kakarot2000. At present I have limited features(find out more by typing !help)\n :)"
+    await ctx.send(text)
+
+@bot.event
+async def on_message(message) :
+    # bot.process_commands(msg) is a couroutine that must be called here since we are overriding the on_message event
+    await bot.process_commands(message) 
+    if str(message.content).lower() == "hello":
+        await message.channel.send('Hi!')
+    
+    if str(message.content).lower() in ['swear_word1','swear_word2']:
+        await message.channel.purge(limit=1)
+
+
+if __name__ == "__main__" :
+    bot.run(DISCORD_TOKEN)
